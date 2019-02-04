adbe2b4b87acf043ae6ebe4bb82d020f60ed7b4b
johnlinder92 <john.linder92@gmail.com>
2019-02-04 18:12:33 +0100

    Skapat klassen customerTest där jag testar om ett customerobject skapats genom att jämföra en arraylist med customerobject innan och efter skapandet.

---
+++ test/customerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class customerTest {
+
+    @Test
+
+    void createCustomer_Returnscustomer_customerListincreases(){
+
+        Customer customer= new Customer();
+
+        
+
+        customerList.add(customer);
+    }
+
+
+}
