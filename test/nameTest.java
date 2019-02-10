b87bbd54e61d5d112a29bdccbb96580dff831cda
johnlinder92 <john.linder92@gmail.com>
2019-02-10 18:28:40 +0100

    Testfunktionaliteten som testar Name.Createname() är klar, testar även om att metoden endast accepterar Stor första bokstav på både förnamn och efternamn.

--- test/nameTest.java
+++ test/nameTest.java
@@ -1,11 +1,30 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class nameTest {
     @Test
    public void createName_Checksifonlycreatesnameswithcapitalfirstletter_returnsTrue()
    {
+       InputStream inputStream = consolehandlerTest.class.getResourceAsStream("/testcreatename.txt");
+       Scanner scanner = new Scanner(inputStream);
+       ConsoleHandler ch = new ConsoleHandler(scanner);
 
-       
+      Name namnet = Name.Createname(ch);
+
+       String efternamn = "Linder";
+        String förnamn = "John";
+      Name namnetb = new Name(förnamn,efternamn);
+
+
+       assertEquals(namnet, namnetb);
+
+
+
      }
 
 
